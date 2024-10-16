0d2c53479a2acb719cce57aad028dd05f1a5b37e
Jordie Biemold <git@jb.bluetainer.nl>
2024-10-16 11:59:04 +0200

    Failure case

--- src/test/java/test/x/TestGraal.java
+++ src/test/java/test/x/TestGraal.java
@@ -9,7 +9,7 @@ import java.nio.file.Path;
 public class TestGraal {
 
     @Test
-    void testGraalPyOpenXl() {
+    void works() {
         try (var ctx = GraalPyResources.createContext()) {
             ctx.eval("python", """
                     import openpyxl
@@ -28,7 +28,35 @@ public class TestGraal {
             for (int i = 0; i < excelOutput.length; i++) {
                 excelOutputSigned[i] = (byte) excelOutput[i];
             }
-            Files.write(Path.of("test.xlsx"), excelOutputSigned);
+            Files.write(Path.of("test-works.xlsx"), excelOutputSigned);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void fails() {
+        try (var ctx = GraalPyResources.createContext()) {
+            ctx.eval("python", """
+                    import openpyxl
+                    import io
+                    from openpyxl.workbook import Workbook # This line fails with the error
+                    
+                    # Typing here makes requires the import above, causing the HostException
+                    wb: Workbook = openpyxl.Workbook()
+                    wb.active['A1'] = 'Hello world!'
+                    
+                    out_bytes = io.BytesIO()
+                    wb.save(out_bytes)
+                    output = out_bytes.getvalue()
+                    """);
+
+            int[] excelOutput = ctx.getBindings("python").getMember("output").as(int[].class);
+            byte[] excelOutputSigned = new byte[excelOutput.length];
+            for (int i = 0; i < excelOutput.length; i++) {
+                excelOutputSigned[i] = (byte) excelOutput[i];
+            }
+            Files.write(Path.of("test-fails.xlsx"), excelOutputSigned);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
