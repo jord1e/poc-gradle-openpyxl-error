02e03e4c7800426f2bd921c631cf46b63654bd64
Jordie Biemold <git@jb.bluetainer.nl>
2024-10-16 11:51:34 +0200

    Test

--- src/test/java/test/x/TestGraal.java
+++ src/test/java/test/x/TestGraal.java
@@ -22,8 +22,13 @@ public class TestGraal {
                     wb.save(out_bytes)
                     output = out_bytes.getvalue()
                     """);
-            byte[] excelOutput = ctx.getBindings("python").getMember("output").as(byte[].class);
-            Files.write(Path.of("test.xlsx"), excelOutput);
+
+            int[] excelOutput = ctx.getBindings("python").getMember("output").as(int[].class);
+            byte[] excelOutputSigned = new byte[excelOutput.length];
+            for (int i = 0; i < excelOutput.length; i++) {
+                excelOutputSigned[i] = (byte) excelOutput[i];
+            }
+            Files.write(Path.of("test.xlsx"), excelOutputSigned);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
